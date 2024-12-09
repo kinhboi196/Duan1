0e34711be2e14cf04dbff671e5ff1d4a46d70e9a
gizom <duclmph51237@gmail.com>
2024-12-09 13:51:15 +0700

    up db

--- duan1.sql
+++ duan1.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Dec 06, 2024 at 11:57 AM
+-- Generation Time: Dec 09, 2024 at 06:50 AM
 -- Server version: 8.0.30
 -- PHP Version: 8.1.10
 
