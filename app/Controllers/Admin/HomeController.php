5bff810571a8c2f063a7834254882d4df66cd1b6
kinhboi196 <kinhboi196@gmail.com>
2024-12-09 14:02:31 +0700

    Update them dashboard

--- app/Controllers/Admin/HomeController.php
+++ app/Controllers/Admin/HomeController.php
@@ -1,11 +1,18 @@
 <?php
-    class HomeController extends ControllerAdmin{
-        public function dashboard() {
-            $productModel = new ProductModel();
-            $listProduct = $productModel->getProductAdminDashboard();
-    
-            include 'app/Views/Admin/index.php';
-        }
-        
+class HomeController extends ControllerAdmin
+{
+    public function dashboard()
+    {
+        $productModel = new ProductModel();
+        $listProduct = $productModel->getProductAdminDashboard();
+
+        include 'app/Views/Admin/index.php';
     }
+    public function showAllProduct()
+    {
+        $producModel = new ProductModel();
+        $listProduct = $producModel->getAllProduct();
+        include 'app/Views/Admin/index.php';
+    }
+}
 ?>
